ea1503ed72e8bb11368a5aefba41862fa5f81374
johnfrontdev2 <138502862+johnfrontdev2@users.noreply.github.com>
2025-09-05 22:29:11 -0300

    Updated testimonials.tsx

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { TestimonialsColumn } from "./testimonials-columns-1";
 import { motion } from "motion/react";
+import ScrollReveal from './ScrollReveal';
  
 const testimonials = [
   {
